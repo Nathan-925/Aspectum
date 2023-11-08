3851026f030a812a04f2605b534cb02115707c9f
Nathan <nathaniel.dunlap925@gmail.com>
2023-11-08 10:52:01 -0500

    Do it all again

--- asp/Camera.cpp
+++ asp/Camera.cpp
@@ -69,10 +69,60 @@ namespace asp{
 	}
 
 	void Camera::cull(vector<Vertex> vertices, forward_list<Triangle> triangles){
-		for(Triangle t: triangles){
+		auto prev = triangles.before_begin();
+		for(auto it = triangles.begin(); it != triangles.end(); prev = it++){
+			Triangle t = *it;
+			int culled[3];
+			int numCulled = 0;
 			for(int i = 0; i < 3; i++){
-				Vector3D p = vertices[t.vertices[i]].position;
+				Vector3D pos = vertices[t.vertices[i]].position;
+				if(abs(pos.x) > 1 || abs(pos.y) > 1 || pos.z < 0)
+					culled[numCulled++] = i;
+				else
+					culled[2-i+numCulled] = i;
+			}
 
+			if(numCulled == 3){
+				triangles.erase_after(prev);
+				it = prev;
+			}
+			else if(numCulled == 2){
+				it->vertices[culled[0]] = vertices.size();
+				double d1 = plane.intersectionPercent(vertices[t.vertices[culled[0]]].position, vertices[t.vertices[culled[2]]].position);
+				vertices.push_back(lerp<Vertex>(d1, vertices[t.vertices[culled[0]]], vertices[t.vertices[culled[2]]]));
+				it->normals[culled[0]] = lerp<Vector3D>(d1, t.normals[culled[0]], t.normals[culled[2]]);
+				//it->texels[culled[0]] = lerp<Vector>(d1, t.texels[culled[0]], t.texels[culled[2]]);
+
+				it->vertices[culled[1]] = vertices.size();
+				double d2 = plane.intersectionPercent(vertices[t.vertices[culled[1]]].position, vertices[t.vertices[culled[2]]].position);
+				vertices.push_back(lerp<Vertex>(d2, vertices[t.vertices[culled[1]]], vertices[t.vertices[culled[2]]]));
+				it->normals[culled[1]] = lerp<Vector3D>(d2, t.normals[culled[1]], t.normals[culled[2]]);
+				//it->texels[culled[1]] = lerp<Vector>(d2, t.texels[culled[1]], t.texels[culled[2]]);
+			}
+			else if(numCulled == 1){
+				Triangle clipping(t);
+
+				clipping.vertices[0] = vertices.size();
+				double d1 = plane.intersectionPercent(vertices[t.vertices[culled[0]]].position, vertices[t.vertices[culled[1]]].position);
+				vertices.push_back(lerp<Vertex>(d1, vertices[t.vertices[culled[0]]], vertices[t.vertices[culled[1]]]));
+				clipping.normals[0] = lerp<Vector3D>(d1, t.normals[culled[0]], t.normals[culled[1]]);
+				//clipping.texels[0] = lerp<Vector>(d1, t.texels[culled[0]], t.texels[culled[1]]);
+
+				clipping.vertices[1] = vertices.size();
+				double d2 = plane.intersectionPercent(vertices[t.vertices[culled[0]]].position, vertices[t.vertices[culled[2]]].position);
+				vertices.push_back(lerp<Vertex>(d2, vertices[t.vertices[culled[0]]], vertices[t.vertices[culled[2]]]));
+				clipping.normals[1] = lerp<Vector3D>(d2, t.normals[culled[0]], t.normals[culled[2]]);
+				//clipping.texels[1] = lerp<Vector>(d2, t.texels[culled[0]], t.texels[culled[2]]);
+
+				clipping.vertices[2] = t.vertices[culled[1]];
+				clipping.normals[2] = t.normals[culled[1]];
+				//clipping.texels[2] = t.texels[culled[1]];
+
+				it->vertices[culled[0]] = clipping.vertices[1];
+				it->normals[culled[0]] = clipping.normals[1];
+				//it->texels[culled[0]] = clipping.texels[1];
+
+				triangles.push_front(clipping);
 			}
 		}
 	}
